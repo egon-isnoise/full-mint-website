007e4d0c629024a4658f7179f1fac6fd40471230
egon-isnoise <egonisnoise@proton.me>
2022-08-05 10:40:29 +0200

    first and only (?)push

---
+++ contracts/RoboPunksNFT.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract RoboPunksNFT is ERC721, Ownable {
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    uint256 maxPerWallet;
+    bool public isPublicMintEnabled;
+    string internal baseTokenUri;
+    address payable public withdrawWallet;
+    mapping(address => uint256) public walletMints;
+
+    constructor() payable ERC721('RoboPunks', 'RP') {
+        mintPrice = 0.02 ether;
+        totalSupply = 0;
+        maxSupply = 1000;
+        maxPerWallet = 3;
+        // set withdraw wallet address
+    }
+
+    function setIsPUblicMintEnabled(bool isPublicMintEnabled_) external onlyOwner{
+        isPublicMintEnabled = isPublicMintEnabled_;
+    }
+
+    function setBaseToken(string calldata baseTokenUri_) external onlyOwner {
+        baseTokenUri = baseTokenUri_;
+    }
+
+    function tokenURI(uint256 tokenId_) public view override returns (string memory){
+        require(_exists(tokenId_), 'Token does not exist!');
+        return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), ".json"));
+    }
+
+    function withdraw() external onlyOwner {
+        (bool success, ) = withdrawWallet.call{ value: address(this).balance}('');
+        require(success, 'withdraw failed!');
+    }
+
+    function mint(uint256 quantity_) public payable {
+        require(isPublicMintEnabled, 'minting not enabled!');
+        require(msg.value == quantity_ * mintPrice, 'wrong mint value');
+        require(totalSupply + quantity_ <= maxSupply, 'Sold out!');
+        require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet');
+
+        for (uint256 i = 0; i < quantity_; i ++){
+            uint256 newTokenId = totalSupply + 1;
+            totalSupply ++;
+            _safeMint(msg.sender, newTokenId);
+        }
+    }
+
+
+}
